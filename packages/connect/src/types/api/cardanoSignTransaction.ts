dd9516562ac3f047ad618dff9233184102bcd00a
Tomáš Martykán <t.martykan@gmail.com>
2023-12-19 12:44:41 +0100

    refactor(connect): validation in `cardano` APIs

--- packages/connect/src/types/api/cardanoSignTransaction.ts
+++ packages/connect/src/types/api/cardanoSignTransaction.ts
@@ -1,15 +1,14 @@
 import type { Params, Response } from '../params';
-import type { CardanoSignTransaction, CardanoSignedTxData } from './cardano';
+import type {
+    CardanoSignTransaction,
+    CardanoSignTransactionExtended,
+    CardanoSignedTxData,
+} from './cardano';
 
 export declare function cardanoSignTransaction(
     params: Params<CardanoSignTransaction>,
 ): Response<CardanoSignedTxData>;
 
 export declare function cardanoSignTransaction(
-    params: Params<
-        CardanoSignTransaction & {
-            unsignedTx: { body: string; hash: string };
-            testnet: boolean;
-        }
-    >,
+    params: Params<CardanoSignTransactionExtended>,
 ): Response<CardanoSignedTxData & { serializedTx: string }>;
